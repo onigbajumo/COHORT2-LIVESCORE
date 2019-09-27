7a025edf0df53ab8ed60c175f2f7a6affddc8467
kmbawuike <kmbawuike@gmail.com>
2019-09-27 17:02:37 +0100

    Initial Commit

---
+++ src/components/Home.js
@@ -0,0 +1,13 @@
+import React, {Component, Fragment} from 'react'
+import '../styles/home.css'
+class Home extends Component{
+  render(){
+    return(
+      <Fragment>
+      
+      </Fragment>
+    )
+  }
+}
+
+export default Home;
