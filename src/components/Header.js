0face92f52a73ec462dbc45aa2e6de248bd0c033
Emmanuel <godsonlive91@gmail.com>
2019-09-30 18:10:58 +0100

    livescore app

---
+++ src/components/Header.js
@@ -0,0 +1,28 @@
+import React, { Component } from "react";
+import logo from "./img/lf.svg";
+
+class Header extends Component {
+  
+  render() {
+    return (
+      <main className="header">
+        <div className="logoheader">
+          <img src={logo} className="logo" />
+          <h2>
+            {" "}
+            <span>LearnFactory</span> Scores{" "}
+          </h2>
+        </div>
+
+        <div className="list">
+          <a> Predict </a>
+          <a> Contact </a>
+          <a> About </a>
+          <a> Services </a>
+        </div>
+    
+      </main>
+    );
+  }
+}
+export default Header;
