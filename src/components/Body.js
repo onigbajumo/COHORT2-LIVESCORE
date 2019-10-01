10ffa685c7cff15be1e8866712e684cd7183b187
kmbawuike <kmbawuike@gmail.com>
2019-10-01 14:32:50 +0100

    Created Body

---
+++ src/components/Body.js
@@ -0,0 +1,8 @@
+import React, {Component} from 'react'
+
+
+class Load extends Component{
+
+}
+
+export default Load;
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,12 @@
 import React, {Component, Fragment} from 'react'
 import '../styles/home.css'
+import Body from './Body'
 class Home extends Component{
   render(){
     return(
       <Fragment>
         <h1>Testing</h1>
+        <Body />
       </Fragment>
     )
   }
